ad7a42410695a87739b02714c3762dd93f9001ff
Haalloobim <withbimatara@gmail.com>
2024-01-15 21:16:55 +0700

    feat: completing loading page

--- src/app/loading.js
+++ src/app/loading.js
@@ -4,7 +4,7 @@ const loading = () => {
   return (
     <div className='min-h-screen w-full flex justify-center items-center bg-slate-100 mx-auto'>
         <div className='p-7 px-10 rounded-xl bg-slate-50 shadow-2xl  flex flex-col items-center gap-3'>
-            <div class="deez-loader"></div>
+            <div className="deez-loader"></div>
             <div className='text-lg font-medium'>
                 Loading...
             </div>
