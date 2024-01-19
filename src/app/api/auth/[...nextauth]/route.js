76448e91801ef63a02fa6404a5120a089ec27171
Haalloobim <withbimatara@gmail.com>
2024-01-19 13:27:50 +0700

    feat: modify git ignore

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,13 @@
 import NextAuth from "next-auth";
+import githubAuth from "next-auth/providers/github";
+
+const authOptions = {
+    providers: [
+        githubAuth(
+            cli
+        )
+    ]
+}
+
 
 const handler = NextAuth()
