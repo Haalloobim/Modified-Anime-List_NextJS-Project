bc6928eb135f429b283745ee1574e0f3b27d3a49
Haalloobim <withbimatara@gmail.com>
2024-01-19 21:39:06 +0700

    feat: add collection base

---
+++ src/app/users/dashboard/Collection.jsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Collection = () => {
+  return (
+    <>
+      <div className="container flex flex-col gap-3 px-10 py-3 mb-12">
+        <div className="flex justify-between items-center">
+          <h1 className="font-roboto font-sans md:text-2xl text-xl font-bold text-[#111111] tracking-normal text-center pb-2 px-3">
+            My Collection
+          </h1>
+          <Link
+            href={"/users/dashboard/collection"}
+            className="text-sm text-indigo-500 underline hover:text-gray-800 transition-all duration-200 ease-in-out"
+          >
+            See all collection...
+          </Link>
+        </div>
+        <div className="flex flex-row gap-x-10 ">
+            <div className="relative min-h-fit border border-gray-500 bg-white rounded-lg">
+                <Image
+                src={"https://placehold.co/400x600.png"}
+                alt=""
+                width={200}
+                height={260}
+                className="inset-0 bg-cover bg-center z-0 rounded-lg"
+                />
+                <div className="absolute inset-0 rounded-lg bottom-0 bg-green-600 z-10 min-h-full flex justify-center items-center opacity-0 hover:opacity-100 transition-all duration-500 ease-in-out">
+                    <h1 className="text-black font-bold text-2xl z-30   ">hello</h1>
+                </div>
+            </div>
+            <div className=" min-h-fit border border-gray-500 bg-white rounded-lg ">
+                <Image
+                src={"https://placehold.co/400x600.png"}
+                alt=""
+                width={200}
+                height={260}
+                className="object-cover rounded-lg"
+                />
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Collection;
--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,13 +1,98 @@
 import React from "react";
+import { authUserSession } from "@/services/auth.services";
+import Link from "next/link";
+import Image from "next/image";
+import Collection from "@/app/users/dashboard/Collection";
 
 const Page = async () => {
   const user = await authUserSession();
-  console.log(user);
 
-  return(
-    <div>
-        
-    </div>
+  const Dashboard = () => {
+    return (
+      <div className="p-6 px-8 bg-slate-100">
+        <div className="container flex flex-col gap-y-5 mx-auto w-full">
+          <div className="text-center">
+            <h1 className="text-xl text-gray-900 font-medium">
+              Hi
+              <span className="text-blue-900 font-semibold"> {user.name}</span>!
+              Welcome to your dashboard.
+            </h1>
+          </div>
+          <div id="content" className="flex w-full  justify-center">
+            <div className="w-fit flex flex-col gap-y-2 bg-white rounded-xl shadow-xl border border-gray-400 py-4 px-8">
+              <div className="p-2 flex justify-center">
+                <Image
+                  src={user.image}
+                  width={150}
+                  height={150}
+                  className="rounded-full border-2 border-gray-800 object-cover"
+                />
+              </div>
+              <div className="text-center">
+                <h1>Users Name : {user.name}</h1>
+                <h1>User Email : {user.email}</h1>
+              </div>
+              <div className="flex flex-row bg-white md:my-2">
+                <div className="h-[2px] flex-1 bg-[#D1D5DC] rounded mx-4"></div>
+              </div>
+              <div className="flex justify-center">
+                <Link
+                  href={"/api/auth/signout"}
+                  className="px-3 py-2 bg-gray-50 hover:bg-white hover:-translate-y-[2px] transition-all border border-gray-400 text-gray-800 rounded-lg shadow-lg"
+                >
+                  Sign Out
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const NotLogin = () => {
+    return (
+      <div className="flex w-full min-h-screen justify-center items-center">
+        <div className="flex flex-col gap-y-3 justify-center text-center bg-white rounded-xl shadow-xl border border-gray-400 px-6 py-10">
+          <h1 className="text-red-600 font-medium text-2xl">
+            You are not logged in! Please login to access this page.
+          </h1>
+          <h2 className="text-blue-500 font-medium text-lg">
+            Back to{" "}
+            <Link
+              href={"/"}
+              className="text-blue-600 underline hover:text-blue-800 transition-all"
+            >
+              Home
+            </Link>{" "}
+            /{" "}
+            <Link
+              href={"/api/auth/signin"}
+              className="text-blue-600 underline hover:text-blue-800 transition-all"
+            >
+              Login
+            </Link>{" "}
+            ...
+          </h2>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      {!user ? (
+        <NotLogin />
+      ) : (
+        <div className="flex flex-col min-h-screen">
+          <Dashboard />
+          <div className="flex flex-row  md:my-2">
+            <div className="h-[2px] flex-1 bg-[#c4c6cb] rounded mx-10"></div>
+          </div>
+          <Collection />
+        </div>
+      )}
+    </>
   );
 };
 
