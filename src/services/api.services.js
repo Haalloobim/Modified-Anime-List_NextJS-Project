1e81b4f3fb10122a3ae8931bca845a5fbdb3bc69
Haalloobim <withbimatara@gmail.com>
2024-01-20 12:24:24 +0700

    fix: fixing minor typo

--- src/services/api.services.js
+++ src/services/api.services.js
@@ -1,5 +1,5 @@
 const getAnimeResponse = async (res, query) => {
-    let apiURL = `${process.env.NEXT_PUBLIC_API_BASSE_URL}${res}?${query}`;
+    let apiURL = `${process.env.NEXT_PUBLIC_API_BASE_URL}${res}?${query}`;
     const response = await fetch(apiURL);
     const topAnimes = await response.json();
     return topAnimes;
